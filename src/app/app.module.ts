99debc66721e75dbc5e1e079449c538115fc4a9a
Pulkit Verma <pulkitverma004@gmail.com>
2017-12-16 11:56:40 +0530

    First Setup Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/core/header/header.component';
+import { FooterComponent } from './modules/core/footer/footer.component';
+import { LandingPageComponent } from './modules/landing-page/landing-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LandingPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
